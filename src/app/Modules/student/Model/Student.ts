dfced00bd9e696dd3ff84e989a37fb0530f49363
Tharindu Jayawardhana <tharindujayawardha1@gmail.com>
2025-06-17 13:28:56 +0530

    create student model

---
+++ src/app/Modules/student/Model/Student.ts
@@ -0,0 +1,10 @@
+export interface Student {
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    currentRegistrationNumber: string;
+    currentCourse: string;
+    createdAt?: string
+}
